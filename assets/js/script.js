4a72b9ec7979131923690ac8eccf46c7d2a0674f
alhadad-xyz <84913031+alhadad-xyz@users.noreply.github.com>
2021-07-23 21:16:12 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,18 @@
+// Hamburger Menu
 const menuToggle = document.querySelector('.header__menu input');
 const nav = document.querySelector('.header__nav');
 
 menuToggle.addEventListener('click', function () {
     nav.classList.toggle('slide');
 });
+
+// Menu Active
+const header = document.querySelector(".header__nav");
+const btns = header.getElementsByClassName("header__link");
+for (let i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  const current = document.getElementsByClassName("header__active");
+  current[0].className = current[0].className.replace(" header__active", "");
+  this.className += " header__active";
+  });
+}
